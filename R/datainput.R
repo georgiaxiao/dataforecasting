93684b69522f66c1ee357ec1b8cb8ecb1a0bc55f
georgiaxiao <75839631+georgiaxiao@users.noreply.github.com>
2020-12-12 01:52:47 +0800

    Update datainput.R

--- R/datainput.R
+++ R/datainput.R
@@ -1,6 +1,7 @@
 #' data_402
 #'@format a vector with 200 elements inside.
 #'@description timeseries
+library(read_xl)
 'data_402'
 data_402=read_xlsx('Dataset for Homework.xlsx',col_names = F)
 usethis::use_data(data_402,overwrite = TRUE)
